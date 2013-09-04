ed6084d9c6790d2d7f15a246a23edf604d0b64e4
Jon Brule <brulejr@gmail.com>
2013-09-04 07:07:27 -0400

    Integrated hapi/lout (doc) plugin. Added support for hapi/tv (debug) plugin.

--- app-init.js
+++ app-init.js
@@ -4,11 +4,39 @@ var appcfg = require("config").WEATHER;
 var log4js = require('log4js');
 var routes = require('./routes');
 
+var configureLout = function(server) {
+	server.pack.require({
+		lout : {
+			endpoint : '/docs'
+		}
+	}, function(err) {
+		if (err) {
+			console.log('Failed loading plugins');
+		}
+	});
+};
+
+var configureTv = function(server) {
+	var debugPort = process.env.DEBUG_PORT || appcfg.debugPort;
+	var tvOptions = {
+		webSocketPort : debugPort,
+		debugEndpoint : '/debug/console',
+		queryKey : 'debug'
+	};
+	server.pack.require('tv', tvOptions, function(err) {
+		if (!err) {
+			server.log([ 'info', 'tv' ], 'Loaded plugin');
+		} else {
+			server.log([ 'error', 'tv' ], 'Failed loading plugin: ' + err.message);
+		}
+	});
+};
+
+
 // configure logging
 log4js.configure('log4js.json', {});
 
 // configure server
-var port = process.env.PORT || appcfg.defaultPort;
 var hapicfg = {
   views: {
     engines: { jade: 'jade' },
@@ -18,7 +46,10 @@ var hapicfg = {
     }
   }
 };
-var server = new Hapi.Server(port, hapicfg);
+var serverPort = process.env.PORT || appcfg.serverPort;
+var server = new Hapi.Server('0.0.0.0', serverPort, hapicfg);
+configureLout(server);
+// configureTv(server);
 server.addRoutes(routes);
 
 // export application configuration
